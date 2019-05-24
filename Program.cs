a959adbdd3b5cc725cc2fcaad3b5a7f5a317389a
Patrick Duff <patrick.duff@bodybuilding.com>
2019-05-24 13:01:54 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace treasure_hunt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
